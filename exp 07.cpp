d42854e251e94c09ff3bf878cc99faac2acbcfe6
dabbukotturaghu <112841745+dabbukotturaghu@users.noreply.github.com>
2023-05-16 12:21:23 +0530

    Add files via upload

---
+++ exp 07.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int freq[26] = {0};
+    char c;
+    while ((c = getchar()) != EOF) {
+        if (c >= 'A' && c <= 'Z') {
+            freq[c - 'A']++;
+        }
+    }
+    int max_i = 0, second_max_i = 0, third_max_i = 0;
+    for (int i = 1; i < 26; i++) {
+        if (freq[i] > freq[max_i]) {
+            third_max_i = second_max_i;
+            second_max_i = max_i;
+            max_i = i;
+        } else if (freq[i] > freq[second_max_i]) {
+            third_max_i = second_max_i;
+            second_max_i = i;
+        } else if (freq[i] > freq[third_max_i]) {
+            third_max_i = i;
+        }
+    }
+    printf("Mapping:\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c -> %c\n", 'A' + i, 'A' + ((i == max_i) ? 'E' : (i == second_max_i) ? 'T' : (i == third_max_i) ? 'A' : ' '));
+    }
+
+    return 0;
+}
